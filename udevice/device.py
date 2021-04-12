8e9c7acbdccad09186c835a29871e1cd0dadfb7a
Adam Bujak <adam@whisper.ai>
2021-04-11 19:13:44 -0700

    update device.py

--- udevice/device.py
+++ udevice/device.py
@@ -1,18 +1,38 @@
 import os
 import time
 import serial
-from threading import Thread, Lock
+from threading import Thread, Lock, Event
 
 class SerialDevice:
     def __init__(self, serialPort, baudRate=115200, readSleepTimeMs=1):
         self.serialMutex = Lock()
         self.readSleepTime = readSleepTimeMs / 1000;
         self.serialInstance = serial.Serial(serialPort, baudRate, timeout=0.5)
+        self.stopReadEvent = Event()
+        self.stopReadEvent.clear()
         self.readThreadInstance = Thread(target=self.read_thread, args=[])
         self.readThreadInstance.start()
 
+    def __del__(self):
+        try:
+            self.readThreadInstance.join()
+        except:
+            pass
+        try:
+            self.stop_read()
+        except:
+            pass
+
+    def stop_read(self):
+        self.stopReadEvent.set()
+
+    def read_stopped(self):
+        return self.stopReadEvent.is_set()
+
     def read_thread(self):
         while 1:
+            if self.read_stopped():
+                break
             readData = self.read_raw_char()
             if readData != '': #empty bytes
                 self.read_callback(readData)
